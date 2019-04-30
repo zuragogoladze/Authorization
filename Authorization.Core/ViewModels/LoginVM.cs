38c74e702502574b2afd7649e3272cd83e4409f1
zuragogoladze <zuragogoladze@github.com>
2019-04-30 20:54:23 +0400

    Edit: 'loginVM class, editted limitations'

--- Authorization.Core/ViewModels/LoginVM.cs
+++ Authorization.Core/ViewModels/LoginVM.cs
@@ -5,9 +5,13 @@ namespace Authorization.Core.ViewModels
     public class LoginVM
     {
         [Required]
+        [StringLength(20, ErrorMessage = "The {0} must be from {2} to {1} characters long", MinimumLength = 5)]
+        [RegularExpression(@"[A-Za-z0-9]+", ErrorMessage = "Username must contain only letters and numbers")]
         public string Username { get; set; }
 
         [Required]
+        [StringLength(20, ErrorMessage = "The {0} must be from {2} to {1} characters long", MinimumLength = 6)]
+        [RegularExpression(@".*[0-9].*", ErrorMessage = "Password must contain at least 1 number")]
         public string Password { get; set; }
         public bool RememberMe { get; set; }
     }
